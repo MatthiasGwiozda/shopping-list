495cc705b97fbd61c0fc87bde5cdf3cbbfeff993
Matthias Gwiozda <matzeqwertz@gmail.com>
2022-02-06 12:39:29 +0100

    [TASK] added comment

--- sql/getStructure.sql
+++ sql/getStructure.sql
@@ -1,3 +1,9 @@
+
+/*
+This query can be used to get the current structure of the database.
+The structure selected by this query can be inserted into the file
+`structure.sql`.
+*/
 SELECT sql || ';' || char(13) AS sql
 	FROM sqlite_schema
 		WHERE sql IS NOT NULL
