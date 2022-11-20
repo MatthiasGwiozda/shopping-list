113d8b238128c252e37931539b4976c7aea2dcfe
Matthias Gwiozda <matzeqwertz@gmail.com>
2022-11-20 12:13:59 +0100

    [TASK] made Component much smaller

--- src/components/Component.ts
+++ src/components/Component.ts
@@ -1,86 +1,15 @@
-import constants from "../scripts/constants";
-import FileUtilities from "../scripts/utilities/FileUtilities";
-import PathUtilities from "../scripts/utilities/PathUtilities";
-import ComponentConstructor from "../scripts/types/components/ComponentConstructor";
-import { ComponentParameters, Components } from "../scripts/types/components/Components";
 
-enum FileType {
-    script = 'scripts.js',
-    html = 'index.html'
-}
+export default abstract class Component {
 
-/**
- * The base - class for every Component.
- * Every Component must "default export" a Class, which extends
- * this Component - class.
- * 
- * The generic part is useful so that a component knows it's parameters, which are stored
- * in the ComponentParameters.
- * 
- * The Component - class additionally takes care of the injection of a component.
- */
-export default abstract class Component<C extends Components> {
-    private static readonly srcPath = 'src/';
-
-    /**
-     * The constructor of a class, which extends the Component - class must use the default - constructor, so
-     * that this two parameters will always be provided.
-     * 
-     * @param container the container, in which the element was loaded.
-     * Every component may be loaded more than once at the same time.
-     * through this parameter the Component is able to identify the actual HTMLElement
-     * for it's "instance".
-     * 
-     * @param componentParameters the parameters, which can be passed into a component
-     */
     constructor(
-        protected container: HTMLElement,
-        protected componentParameters: ComponentParameters[C]
-    ) { }
-    /**
-     * this function will be called once when a Component was rendered in the dom.
-     */
-    public abstract rendered(): void;
-
-    /**
-     * All components have a fixed structure. Through this function one can access
-     * the Files, which are included in a component.
-     */
-    private getComponentFilePath(component: Components, fileType: FileType) {
-        const folder = fileType == FileType.script ? 'lib/' : Component.srcPath
-        return `${folder}${constants.componentsFolderName}/${component}/${fileType}`;
+        protected container: HTMLElement
+    ) {
+        this.injectHtmlToElement();
     }
 
-    /**
-     * This function injects the scripts.ts - file of the component, which is currently active.
-     * Note that the script will not be "unloaded", when the component is removed from the dom.
-     */
-    private injectComponentScript<T extends Component<any>>(component: Components, componentParameter): T {
-        const scriptPath = PathUtilities.getPath(this.getComponentFilePath(component, FileType.script));
-        /**
-         * the type is a reference to the constructor of the Components - class.
-         */
-        const componentClass: ComponentConstructor<T, any> = require(scriptPath).default;
-        return new componentClass(this.container, componentParameter);
+    private injectHtmlToElement() {
+        this.container.innerHTML = this.getHtmlTemplate();
     }
 
-    private injectHtmlToElement(component: Components) {
-        const html = FileUtilities.getFileContent(this.getComponentFilePath(component, FileType.html));
-        this.container.innerHTML = html;
-    }
-
-    /**
-     * In general this function allows the user to 
-     * insert a component into the given htmlElement.
-     * This function may for example be used in a component to inject other components.
-     * @returns the instance of the component, which was defined in the components - parameter.
-     */
-    public injectComponent<C extends Components>(
-        component: C, componentParameters?: ComponentParameters[C]
-    ): Component<C> {
-        this.injectHtmlToElement(component);
-        const instance = this.injectComponentScript(component, componentParameters);
-        instance.rendered();
-        return instance;
-    }
+    protected abstract getHtmlTemplate(): string;
 }
