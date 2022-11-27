43df32974dc3093bb54fd4707d224cef734e601c
Matthias Gwiozda <matzeqwertz@gmail.com>
2022-11-27 13:11:38 +0100

    [REFACTOR] moved protected function after constructor

--- src/components/Component.ts
+++ src/components/Component.ts
@@ -6,10 +6,10 @@ export default abstract class Component {
     ) {
         this.injectHtmlToElement();
     }
-
+    
+    protected abstract getHtmlTemplate(): string;
+    
     private injectHtmlToElement() {
         this.container.innerHTML = this.getHtmlTemplate();
     }
-
-    protected abstract getHtmlTemplate(): string;
 }
