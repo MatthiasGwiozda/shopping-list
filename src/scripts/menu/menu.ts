4fca7e81ad2bfab59bf14f0179fb392cf980069c
Matthias Gwiozda <matzeqwertz@gmail.com>
2022-12-24 13:36:27 +0100

    [TASK] created Menu - class

--- src/scripts/menu/menu.ts
+++ src/scripts/menu/menu.ts
@@ -3,80 +3,80 @@ import { Components } from '../types/components/Components';
 import constants from '../constants';
 import ApplicationMenuRoutes from './ApplicationMenuRoutes';
 
-/**
- * creates the menu - div element.
- */
-function createMenu() {
-    const menu = document.createElement('div');
-    menu.id = constants.menuId;
-    document.getElementById(constants.containerId).prepend(menu);
-}
+export default class Menu {
 
-function setActiveMenuItem(routeEl: HTMLElement) {
-    const activeClass = 'active';
-    const anchors = document.querySelectorAll(`#${constants.menuId} > a`);
-    anchors.forEach(anchor => {
-        anchor.classList.remove(activeClass);
-    })
-    routeEl.classList.add(activeClass);
-}
+    /**
+     * creates the menu and injects it into the
+     * document.
+     * additionally opens the default component
+     */
+    public injectMenuElements() {
+        this.createMenuDivElement();
+        this.createMenuRouteElements();
+        const menuRouteElements = ApplicationMenuRoutes.map(componentRoute => componentRoute.htmlElement);
+        menuRouteElements.forEach(
+            node => document.getElementById(constants.menuId).appendChild(node)
+        );
+        // now open the default - component
+        this.goToRoute(Components.shoppingList);
+        this.refreshReadyMenuComponents();
+    }
 
-function goToRoute(component: Components) {
-    Component.injectComponent(component, document.getElementById(constants.contentId));
-    const { htmlElement } = ApplicationMenuRoutes.find(componentRoute => componentRoute.component == component)
-    setActiveMenuItem(htmlElement);
-}
-
-function createMenuRouteElements() {
-    ApplicationMenuRoutes.forEach(componentRoute => {
-        const { component, icon, name } = componentRoute;
-        const routeEl = document.createElement('a');
-        routeEl.onclick = () => {
-            goToRoute(component);
-        }
-        routeEl.innerHTML = `<span class='icon'>${icon}</span>` + name;
-        componentRoute.htmlElement = routeEl;
-    });
-}
-
-/**
- * The menu indicates wether certain menu - elements may be used.
- * This function checks, if the menu elements may be used and sets
- * the ready - state for each menu element.
- */
-export async function refreshReadyMenuComponents() {
-    for (const componentRoute of ApplicationMenuRoutes) {
-        const { componentReadyChecks, componentReadyCheckMessage } = componentRoute;
-        if (componentReadyChecks != null) {
-            const promises = componentReadyChecks.map(readyCheck => readyCheck());
-            const results = await Promise.all(promises);
-            const componentIsReady = results.every(result => result);
-            const { htmlElement } = componentRoute;
-            const menuNotReadyClass = 'notReady';
-            if (componentIsReady) {
-                htmlElement.classList.remove(menuNotReadyClass);
-                htmlElement.removeAttribute('title');
-            } else {
-                htmlElement.classList.add(menuNotReadyClass);
-                htmlElement.title = componentReadyCheckMessage;
+    /**
+     * The menu indicates wether certain menu - elements may be used.
+     * This function checks, if the menu elements may be used and sets
+     * the ready - state for each menu element.
+     */
+    public async refreshReadyMenuComponents() {
+        for (const componentRoute of ApplicationMenuRoutes) {
+            const { componentReadyChecks, componentReadyCheckMessage } = componentRoute;
+            if (componentReadyChecks != null) {
+                const promises = componentReadyChecks.map(readyCheck => readyCheck());
+                const results = await Promise.all(promises);
+                const componentIsReady = results.every(result => result);
+                const { htmlElement } = componentRoute;
+                const menuNotReadyClass = 'notReady';
+                if (componentIsReady) {
+                    htmlElement.classList.remove(menuNotReadyClass);
+                    htmlElement.removeAttribute('title');
+                } else {
+                    htmlElement.classList.add(menuNotReadyClass);
+                    htmlElement.title = componentReadyCheckMessage;
+                }
             }
         }
     }
-}
 
-/**
- * creates the menu and injects it into the
- * document.
- * additionally opens the default component
- */
-export function injectMenuElements() {
-    createMenu();
-    createMenuRouteElements();
-    const menuRouteElements = ApplicationMenuRoutes.map(componentRoute => componentRoute.htmlElement);
-    menuRouteElements.forEach(
-        node => document.getElementById(constants.menuId).appendChild(node)
-    );
-    // now open the default - component
-    goToRoute(Components.shoppingList);
-    refreshReadyMenuComponents();
+    private createMenuDivElement() {
+        const menu = document.createElement('div');
+        menu.id = constants.menuId;
+        document.getElementById(constants.containerId).prepend(menu);
+    }
+
+    private createMenuRouteElements() {
+        ApplicationMenuRoutes.forEach(componentRoute => {
+            const { component, icon, name } = componentRoute;
+            const routeEl = document.createElement('a');
+            routeEl.onclick = () => {
+                this.goToRoute(component);
+            }
+            routeEl.innerHTML = `<span class='icon'>${icon}</span>` + name;
+            componentRoute.htmlElement = routeEl;
+        });
+    }
+
+    private goToRoute(component: Components) {
+        Component.injectComponent(component, document.getElementById(constants.contentId));
+        const { htmlElement } = ApplicationMenuRoutes.find(componentRoute => componentRoute.component == component)
+        this.setActiveMenuItem(htmlElement);
+    }
+
+    private setActiveMenuItem(routeEl: HTMLElement) {
+        const activeClass = 'active';
+        const anchors = document.querySelectorAll(`#${constants.menuId} > a`);
+        anchors.forEach(anchor => {
+            anchor.classList.remove(activeClass);
+        })
+        routeEl.classList.add(activeClass);
+    }
 }
