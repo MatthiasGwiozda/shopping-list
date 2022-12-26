0668f1fd6148c4d12dcb9db8375c9ebbda1766e7
Matthias Gwiozda <matzeqwertz@gmail.com>
2022-12-26 09:05:14 +0100

    [TASK] implemented observerSubject functions in MenuObserverComponent

---
+++ src/scripts/menu/MenuObserverComponent.ts
@@ -0,0 +1,17 @@
+import Component from "../../components/Component";
+import Observer from "../types/observer/Observer";
+import ObserverSubject from "../types/observer/ObserverSubject";
+
+export default abstract class MenuObserverComponent extends Component implements ObserverSubject {
+    private observers: Observer[];
+
+    public notifyObservers() {
+        this.observers.forEach(
+            observer => observer.observerSubjectUpdated()
+        )
+    };
+
+    public registerObserver(observer: Observer) {
+        this.observers.push(observer);
+    };
+}
