1faa30596925bcf3e7a8dc4e9333cd856fb486c8
Matthias Gwiozda <matzeqwertz@gmail.com>
2022-12-26 08:52:03 +0100

    [TASK] created MenuObserverComponent class

---
+++ src/scripts/menu/types/MenuObserverComponent.ts
@@ -0,0 +1,8 @@
+import Component from "../../../components/Component";
+import Observer from "../../types/observer/Observer";
+import ObserverSubject from "../../types/observer/ObserverSubject";
+
+export default abstract class MenuObserverComponent extends Component implements ObserverSubject {
+    abstract notifyObservers: () => void;
+    abstract registerObserver: (observer: Observer) => void;
+}
