73c5581f6005f4744bc37d8bf4b23359e626b6b3
Matthias Gwiozda <matzeqwertz@gmail.com>
2023-01-01 11:44:37 +0100

    [REFACTOR] added carriage return to QueryExecuterDao

--- src/scripts/database/dataAccessObjects/QueryExecutorDao.ts
+++ src/scripts/database/dataAccessObjects/QueryExecutorDao.ts
@@ -1,6 +1,7 @@
 import QueryExecutor from "../queryExecutor/QueryExecutor";
 
 export default abstract class QueryExecutorDao {
+
     constructor(
         protected queryExecutor: QueryExecutor
     ) { }
