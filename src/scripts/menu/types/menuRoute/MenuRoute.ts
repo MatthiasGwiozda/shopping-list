e221f5dc9c8b31f9629715cf1ae9d7083a0c14af
Matthias Gwiozda <matzeqwertz@gmail.com>
2023-01-06 10:58:02 +0100

    [TASK] use NamedIcon in MenuRoute

--- src/scripts/menu/types/menuRoute/MenuRoute.ts
+++ src/scripts/menu/types/menuRoute/MenuRoute.ts
@@ -1,9 +1,11 @@
 import MenuComponentFactories from "../../../factories/components/menuComponents/interfaces/MenuComponentFactories";
 import MenuRouteReadyCheck from "../readyCheck/MenuRouteReadyCheck";
+import NamedIcon from "./NamedIcon";
 
 export default interface MenuRoute<
     FactoryType extends MenuComponentFactories
 > {
+    namedIcon: NamedIcon;
     name: string;
     componentFactory: FactoryType;
     icon: string;
