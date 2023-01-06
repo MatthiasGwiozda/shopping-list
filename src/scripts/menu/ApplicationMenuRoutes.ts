636c2dd24541b3e684542ccb67758b785fc5d9d3
Matthias Gwiozda <matzeqwertz@gmail.com>
2023-01-06 11:15:12 +0100

    [TASK] added behavior property to MenuRoute

--- src/scripts/menu/ApplicationMenuRoutes.ts
+++ src/scripts/menu/ApplicationMenuRoutes.ts
@@ -9,6 +9,7 @@ import ShopsFactory from '../factories/components/menuComponents/implementations
 import MealsFactory from '../factories/components/menuComponents/implementations/MealsFactory';
 import MenuComponentFactories from '../factories/components/menuComponents/interfaces/MenuComponentFactories';
 import NamedIcon from './types/menuRoute/NamedIcon';
+import MenuRouteBehavior from './types/menuRoute/MenuRouteBehavior';
 
 function hasAtLeasOneElement(arr: any[]): boolean {
     return arr.length > 0;
@@ -37,6 +38,16 @@ const componentReadyChecks: ComponentReadyChecks = {
 const ApplicationMenuRoutes: MenuRoute<MenuComponentFactories>[] = [
     {
         namedIcon: new NamedIcon('Shopping List', '📝'),
+        behavior: new MenuRouteBehavior(
+            new ShoppingListFactory(),
+            {
+                checks: [
+                    componentReadyChecks.items,
+                    componentReadyChecks.shops
+                ],
+                message: 'Please add items and at least one shop to generate shopping lists'
+            }
+        ),
         componentFactory: new ShoppingListFactory(),
         readyCheck: {
             checks: [
@@ -48,6 +59,15 @@ const ApplicationMenuRoutes: MenuRoute<MenuComponentFactories>[] = [
     },
     {
         namedIcon: new NamedIcon('Items', constants.icons.item),
+        behavior: new MenuRouteBehavior(
+            new ItemsFactory(),
+            {
+                checks: [
+                    componentReadyChecks.categories
+                ],
+                message: 'Please add categories before you add items'
+            }
+        ),
         componentFactory: new ItemsFactory(),
         readyCheck: {
             checks: [
@@ -58,10 +78,22 @@ const ApplicationMenuRoutes: MenuRoute<MenuComponentFactories>[] = [
     },
     {
         namedIcon: new NamedIcon('Categories', constants.icons.category),
+        behavior: new MenuRouteBehavior(
+            new CategoriesFactory(),
+        ),
         componentFactory: new CategoriesFactory(),
     },
     {
         namedIcon: new NamedIcon('Shops', constants.icons.shop),
+        behavior: new MenuRouteBehavior(
+            new ShopsFactory(),
+            {
+                checks: [
+                    componentReadyChecks.categories
+                ],
+                message: 'Please add categories before you define shops. Every shop may have it\'s own order for categories'
+            }
+        ),
         componentFactory: new ShopsFactory(),
         readyCheck: {
             checks: [
@@ -72,6 +104,15 @@ const ApplicationMenuRoutes: MenuRoute<MenuComponentFactories>[] = [
     },
     {
         namedIcon: new NamedIcon('Meals', '🥗'),
+        behavior: new MenuRouteBehavior(
+            new MealsFactory(),
+            {
+                checks: [
+                    componentReadyChecks.itemsWithFood
+                ],
+                message: 'Please add at least one "food - item" to create meals'
+            }
+        ),
         componentFactory: new MealsFactory(),
         readyCheck: {
             checks: [
--- src/scripts/menu/types/menuRoute/MenuRoute.ts
+++ src/scripts/menu/types/menuRoute/MenuRoute.ts
@@ -1,11 +1,14 @@
 import MenuComponentFactories from "../../../factories/components/menuComponents/interfaces/MenuComponentFactories";
 import MenuRouteReadyCheck from "../readyCheck/MenuRouteReadyCheck";
+import MenuRouteBehavior from "./MenuRouteBehavior";
 import NamedIcon from "./NamedIcon";
 
 export default interface MenuRoute<
     FactoryType extends MenuComponentFactories
 > {
     namedIcon: NamedIcon;
+    behavior: MenuRouteBehavior<MenuComponentFactories>;
+    
     componentFactory: FactoryType;
     readyCheck?: MenuRouteReadyCheck;
 }
