1fb84ea8a2a772032c9b16960881ca84f4ff0a20
MatthiasGwiozda <matzeqwertz@gmail.com>
2025-07-31 18:02:26 +0200

    Created Database test

---
+++ src/test/specs/database/Database.test.ts
@@ -0,0 +1,12 @@
+import { test } from "node:test";
+import { equal } from "node:assert";
+import Database from "../../../scripts/database/Database";
+
+test.skip("Database", async (t) => {
+
+    await t.test("run query generateShoppingList", async () => {
+        await Database.initializeDatabase();
+        const result = await Database.generateShoppingList(1, true);
+        equal(result.length > 0, true);
+    })
+})
